2138a4c9d95bab2024156bebe9cf5f880f6130a9
Naresh123se <naresh132na@gmail.com>
2025-01-06 01:14:49 +0545

    changes db table

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,21 +27,10 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'phone' => fake()->phoneNumber(),
-            'address'=>fake()->address(),
-            'role'=>fake()->randomElement(['user']),
-            // 'email_verified_at' => now(),
+            'address' => fake()->address(),
+            'role' => fake()->randomElement(['user']),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    // public function unverified(): static
-    // {
-    //     return $this->state(fn (array $attributes) => [
-    //         'email_verified_at' => null,
-    //     ]);
-    // }
 }
